567a4d3d32211ceb3aeb72effb1c39b5be28fed2
Volodya Bykov <volodyabykov@192.168.1.35>
2024-12-10 17:55:05 +0300

    added build script

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@ const path = require("path");
 module.exports = {
   entry: "./src/main.ts",
   mode: "development",
+  target: "node",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[content-hash].bundle.ts",
+    filename: "[hash].bundle.js",
   },
   module: {
     rules: [
